295c069aa8f5a16957197e0c5c4adf6a03867dec
Тимоха <surefeur@yandex.ru>
2026-06-27 21:48:24 +0300

    change: idk

--- db_test.py
+++ db_test.py
@@ -4,4 +4,9 @@ conn = psycopg.connect("postgresql://postgres:postgres@localhost:5432/cal")
 with conn.cursor() as cur:
 		cur.execute("SELECT version();")
 		print(cur.fetchone())
-conn.close
+		cur.execute("SELECT current_user, current_database();")
+		print(cur.fetchone())
+
+		cur.execute("CREATE TABLE IF NOT EXISTS products (id SERIAL PRIMARY KEY, name TEXT NOT NULL, proteins REAL NOT NULL, fat REAL NOT NULL, carbs REAL NOT NULL);")
+conn.commit()
+conn.close()
